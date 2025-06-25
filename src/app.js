c9ed54a56cce7677deaa248a691a23ba5a4f8375
Luka Lattanzi <lattanziluka203@gmail.com>
2025-06-25 19:35:51 -0300

    Commit 1 25/6/2025

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 // Importa el framework Express para crear el servidor
 const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
 
-// Importa el router de productos y de carritos 
+// Importa el router de productos y de carritos
 const productsRouter = require("./routes/products.router");
 const cartsRouter = require("./routes/carts.router");
+
+// Importa el ProductManager para manejar los productos
+const ProductManager = require("./managers/ProductManager");
+const productManager = new ProductManager("./src/data/products.json");
 
 // Crea una instancia de la aplicación Express
 const app = express();
@@ -11,14 +17,38 @@ const app = express();
 // Define el puerto donde se ejecutará el servidor
 const PORT = 8080;
 
+// Configuración de Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../views"));
+
+// Servir archivos estáticos de la carpeta public
+app.use(express.static(path.join(__dirname, "../public")));
+
 // Middlewares para procesar JSON y datos de formularios
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    console.log('Body:', req.body);
-    next();
+// // Middleware de logging simple
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     console.log('Body:', req.body);
+//     next();
+// });
+
+// Ruta home que muestra los productos usando Handlebars
+app.get("/", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    res.render("home", { products });
+  } catch (error) {
+    res.status(500).render("error", {
+      statusCode: 500,
+      message: "Error al cargar productos",
+      error: error.message,
+      stack: error.stack,
+    });
+  }
 });
 
 // Rutas principales de la API, endpoint /api/products y /api/carts
@@ -27,5 +57,5 @@ app.use("/api/carts", cartsRouter);
 
 // Inicia el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
